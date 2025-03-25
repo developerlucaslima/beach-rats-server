886f3952f6b2f2329a843f6bdc435964ac5948f7
developerlucaslima <developerlucaslima@gmail.com>
2025-03-25 12:39:20 -0300

    :art: refactor: clean up imports and formatting in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from "@/graphql/schema"
-import { resolvers } from "@/graphql/resolvers"
-import { env } from './config/env';
+import { startStandaloneServer } from '@apollo/server/standalone'
 
-const server = new ApolloServer({ resolvers, typeDefs });
+import { resolvers } from '@/graphql/resolvers'
+import { typeDefs } from '@/graphql/schema'
+
+import { env } from './config/env'
+
+const server = new ApolloServer({ resolvers, typeDefs })
 const { url } = await startStandaloneServer(server, {
   listen: { port: env.PORT },
-});
-console.log(`🚀  Server ready at: ${url}`);
+})
+
+console.log(`🚀  Server ready at: ${url}`)
