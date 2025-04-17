d6715f65a31b6b75eb6a725abf5118e7f5d7f7df
developerlucaslima <developerlucaslima@gmail.com>
2025-04-17 11:29:14 -0300

    chore: remove unused configuration files and code

--- src/repositories/players-repository.ts
+++
@@ -1,8 +0,0 @@
-import type { Player, Prisma } from '@prisma/client'
-
-export interface PlayersRepository {
-  createOnePlayer(data: Prisma.PlayerUncheckedCreateInput): Promise<Player>
-  findManyPlayers(): Promise<Player[] | null>
-  findOnePlayerById(id: string): Promise<Player | null>
-  findOnePlayerByEmail(email: string): Promise<Player | null>
-}
