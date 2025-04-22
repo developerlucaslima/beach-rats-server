3215ac7957e6dde1e324b19cdac36f77f5aaaf60
developerlucaslima <developerlucaslima@gmail.com>
2025-04-22 18:42:12 -0300

    feat: add route for Google player authentication

--- src/http/routes/players-routes.ts
+++ src/http/routes/players-routes.ts
@@ -2,10 +2,12 @@ import type { FastifyInstance } from 'fastify'
 import { refreshTokenController } from '../controllers/refresh-token-controller'
 import { signInPlayerController } from '../controllers/sign-in-player-controller'
 import { signUpPlayerController } from '../controllers/sign-up-player-controller'
+import { authPlayerWithGoogleController } from '../controllers/auth-player-with-google-controller'
 
 export async function playerRoutes(app: FastifyInstance) {
 	app.post('/sign-up-player', signUpPlayerController)
 	app.post('/sign-in-player', signInPlayerController)
+	app.post("/auth-player-google", authPlayerWithGoogleController)
 
 	/** Token Refresh */
 	app.patch('/refresh-token-player', refreshTokenController)
