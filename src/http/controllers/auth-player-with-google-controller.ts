44ec7da0d2996e9e61452dc23a802dad885638f7
developerlucaslima <developerlucaslima@gmail.com>
2025-04-22 18:39:50 -0300

    feat: add Google authentication controller and service for player authentication

---
+++ src/http/controllers/auth-player-with-google-controller.ts
@@ -0,0 +1,74 @@
+import type { FastifyReply, FastifyRequest } from "fastify"
+import { z } from "zod"
+
+import { ACCESS_TOKEN_EXPIRATION_SECONDS, REFRESH_TOKEN_COOKIE_NAME, REFRESH_TOKEN_EXPIRATION_SECONDS } from "../jwt/jwt-config"
+import { mapAuthenticatedPlayerResponse } from "../dto/player-dto"
+import { setTokenCookie } from "../jwt/set-refresh-token-cookie"
+import { makeAuthPlayerWithGoogle } from "@/core/factories/make-auth-player-with-google"
+import { GoogleAuthService } from "@/infrastructure/services/google-auth-service"
+
+const authPlayerWithGoogleSchema = z.object({
+  idToken: z.string().min(10),
+})
+
+export async function authPlayerWithGoogleController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const { idToken } = authPlayerWithGoogleSchema.parse(request.body)
+
+  const googleAuthService = new GoogleAuthService()
+  const {
+    sub,
+    email,
+    emailVerified,
+    name,
+    avatarUrl
+  } = await googleAuthService.verifyIdToken(idToken)
+
+  /* Optional: reject unverified emails */
+  if (!emailVerified) {
+    return reply.status(401).send({ message: "Google email not verified." })
+  }
+
+  const authPlayerWithGoogleUseCase = makeAuthPlayerWithGoogle()
+  const { player } = await authPlayerWithGoogleUseCase.execute({ name, email, googleId: sub, avatarUrl })
+
+  const jwtPayload = {
+    role: player.role,
+    subscriptionPlan: player.subscriptionPlan,
+  }
+
+  const token = await reply.jwtSign(
+    jwtPayload,
+    {
+      sign: {
+        sub: player.id,
+        expiresIn: `${ACCESS_TOKEN_EXPIRATION_SECONDS}s`,
+      },
+    },
+  )
+
+  const refreshToken = await reply.jwtSign(
+    jwtPayload,
+    {
+      sign: {
+        sub: player.id,
+        expiresIn: `${REFRESH_TOKEN_EXPIRATION_SECONDS}s`,
+      },
+    },
+  )
+
+  setTokenCookie({
+    reply,
+    tokenName: REFRESH_TOKEN_COOKIE_NAME,
+    token: refreshToken,
+    maxAge: REFRESH_TOKEN_EXPIRATION_SECONDS,
+  })
+
+  return reply.status(201).send({
+    message: 'Auth with google successfully.',
+    token,
+    player: mapAuthenticatedPlayerResponse(player),
+  })
+}
