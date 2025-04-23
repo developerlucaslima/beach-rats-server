ecf980427e38b14a2b4d8ca7ac1a37b1e31a6686
developerlucaslima <developerlucaslima@gmail.com>
2025-04-23 12:48:39 -0300

    feat: implement UpdatePlayerProfileUseCase for updating player profiles and handling not found errors

---
+++ src/core/use-cases/update-player-profile-use-case.ts
@@ -0,0 +1,49 @@
+import type { Player } from "@prisma/client";
+
+import type { IPlayersRepository } from "@/infrastructure/repositories/interfaces/players-repository";
+
+import { ResourceNotFoundException } from "../errors/resource-not-found-exception";
+
+interface UpdatePlayerProfileUseCaseRequest {
+  playerId: string
+  name?: string
+  avatarUrl?: string
+  bio?: string
+  age?: number
+  countryCode?: string
+  latitude?: number
+  longitude?: number
+  mainModalityId?: string
+  physicalConditionName?: string
+  mentalConditionName?: string
+}
+
+interface UpdatePlayerProfileUseCaseResponse {
+  player: Omit<Player, 'passwordHash'>
+}
+
+export class UpdatePlayerProfileUseCase {
+  constructor(private readonly playersRepo: IPlayersRepository) { }
+
+  async execute({
+    playerId,
+    ...data
+  }: UpdatePlayerProfileUseCaseRequest): Promise<UpdatePlayerProfileUseCaseResponse> {
+    // It should get a player by ID.
+    const byId = await this.playersRepo.findById(playerId)
+
+    // It should throw error when player not found by ID.
+    if (!byId) {
+      throw new ResourceNotFoundException('Player')
+    }
+
+    // It should update player with provided data.
+    const updatedPlayer = await this.playersRepo.updateProfile({ playerId, data })
+
+    // It should return player without passwordHash.
+    const { passwordHash, ...safePlayer } = updatedPlayer
+    return {
+      player: safePlayer,
+    }
+  }
+}
