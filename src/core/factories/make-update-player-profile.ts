964f6af671a44bc8e4d8975a732f47276d1b515a
developerlucaslima <developerlucaslima@gmail.com>
2025-04-26 23:53:57 -0300

    refactor: update import paths for player-related use cases to use absolute paths

--- src/core/factories/make-update-player-profile.ts
+++ src/core/factories/make-update-player-profile.ts
@@ -1,9 +1,9 @@
-import { PrismaPlayersRepository } from "@/repositories/prisma/players-repository";
-import { UpdatePlayerProfileUseCase } from "../use-cases/update-player-profile-use-case";
+import { PrismaPlayersRepository } from "@repositories/prisma/players-repository";
+import { UpdatePlayerProfileUseCase } from "@use-cases/update-player-profile-use-case";
 
 export function makeUpdatePlayerProfile() {
   const prismaPlayersRepository = new PrismaPlayersRepository();
-  const updatePlayerProfileUseCase = new UpdatePlayerProfileUseCase(prismaPlayersRepository);
 
+  const updatePlayerProfileUseCase = new UpdatePlayerProfileUseCase(prismaPlayersRepository);
   return updatePlayerProfileUseCase;
 }
