9e06dfed7d873816496c9db01def16b5a221bbd7
developerlucaslima <developerlucaslima@gmail.com>
2025-04-27 19:11:56 -0300

    refactor: extend custom exceptions from BaseException and add UnauthorizedException

--- src/shared/errors/email-not-available-exception.ts
+++ src/shared/errors/email-not-available-exception.ts
@@ -1,6 +1,8 @@
+import { BaseException } from './base-exception'
+
 const CONFLICT_ERROR_CODE = 409
 
-export class EmailNotAvailableException extends Error {
+export class EmailNotAvailableException extends BaseException {
   public readonly code: number
 
   constructor() {
--- src/shared/errors/invalid-credentials-exception.ts
+++ src/shared/errors/invalid-credentials-exception.ts
@@ -1,6 +1,8 @@
+import { BaseException } from './base-exception'
+
 const INVALID_CREDENTIALS_ERROR_CODE = 401
 
-export class InvalidCredentialsException extends Error {
+export class InvalidCredentialsException extends BaseException {
   public readonly code: number
 
   constructor() {
