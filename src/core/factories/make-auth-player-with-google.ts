94c84cb5f47fcfcc2d7e27878eb2f0fb35fc9537
developerlucaslima <developerlucaslima@gmail.com>
2025-04-26 23:54:36 -0300

    refactor: update import path for AuthPlayerWithGoogleUseCase to use absolute path

--- src/core/factories/make-auth-player-with-google.ts
+++ src/core/factories/make-auth-player-with-google.ts
@@ -1,5 +1,5 @@
 import { PrismaPlayersRepository } from "@/infrastructure/repositories/prisma/players-repository"
-import { AuthPlayerWithGoogleUseCase } from "../use-cases/auth-player-with-google-use-case"
+import { AuthPlayerWithGoogleUseCase } from "@use-cases/auth-player-with-google-use-case"
 
 export function makeAuthPlayerWithGoogle() {
   const prismaPlayersRepository = new PrismaPlayersRepository()
