b578688c4f11414dc3866cc6f73e28fa344d883a
developerlucaslima <developerlucaslima@gmail.com>
2025-05-16 17:59:31 -0300

    fix: remove duplicate import of signOutPlayerController in auth-routes

--- src/http/routes/auth-routes.ts
+++ src/http/routes/auth-routes.ts
@@ -1,10 +1,11 @@
+import { authPlayerWithGoogleOneTapController } from '@controllers/auth-player-with-google-one-tap-controller'
+import { signOutPlayerController } from '@controllers/sign-out-player-controller'
 import type { FastifyInstance } from 'fastify'
 
 import { authPlayerWithGoogleController } from '../controllers/auth-player-with-google-controller'
 import { refreshTokenController } from '../controllers/refresh-token-controller'
 import { signInPlayerController } from '../controllers/sign-in-player-controller'
 import { signUpPlayerController } from '../controllers/sign-up-player-controller'
-import { signOutPlayerController } from '@controllers/sign-out-player-controller'
 
 export async function authRoutes(app: FastifyInstance) {
   /** Authenticate */
@@ -12,6 +13,7 @@ export async function authRoutes(app: FastifyInstance) {
   app.post('/sign-in', signInPlayerController)
   app.post('/sign-out', signOutPlayerController)
   app.post('/google', authPlayerWithGoogleController)
+  app.post('/google-one-tap', authPlayerWithGoogleOneTapController)
 
   /** Token Refresh */
   app.patch('/refresh-token', refreshTokenController)
